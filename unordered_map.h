d988df5a79d44adc201e4ffaecb8173304bc8f6b
Aphanasiy <M79150306109@ya.ru>
2018-03-20 19:08:53 +0300

    Added using

--- unordered_map.h
+++ unordered_map.h
@@ -5,50 +5,56 @@
 #include <iostream>
 
 
-const size_t HashMapBaseSize = 11;
+const size_t HashMapStorageSize = 11;
 
 template<class KeyType,
          class ValueType,
          class Hash = std::hash<KeyType> > 
 class HashMap {
-    std::list<std::pair<const KeyType, ValueType>> iter;
-    std::vector<std::vector<typename std::list<std::pair<const KeyType, ValueType>>::iterator>> base;
+  private:
+    const size_t StorageResizeMultiplier = 2;
+    using Key_Value = std::pair<const KeyType, ValueType>;
+    using HashMapList = std::list<Key_Value>;
+    using HashMapStorage = std::vector<std::vector<typename HashMapList::iterator>>;
+
+    HashMapList iter;
+    HashMapStorage storage;
     Hash hasher;
     size_t sz = 0;
 
   public:
-
-    using iterator = typename std::list<std::pair<const KeyType, ValueType>>::iterator;
-    using const_iterator = typename std::list<std::pair<const KeyType, ValueType>>::const_iterator;
+    using iterator = typename HashMapList::iterator;
+    using const_iterator = typename HashMapList::const_iterator;
    
     HashMap(Hash _hasher = Hash())
     : hasher(_hasher) {
-        base = std::vector<std::vector<typename std::list<std::pair<const KeyType, ValueType>>::iterator>>(HashMapBaseSize);
+        storage = HashMapStorage(HashMapStorageSize);
     }
     template <class InpIt>
     HashMap(const InpIt begin,
             const InpIt end,
             Hash _hasher = Hash()) 
     : hasher(_hasher) {
-        base = std::vector<std::vector<typename std::list<std::pair<const KeyType, ValueType>>::iterator>>(HashMapBaseSize);
+        storage = HashMapStorage(HashMapStorageSize);
         for (auto it = begin; it != end; ++it) {
-            size_t key_hash = hasher(it->first) % base.size();
+            size_t key_hash = hasher(it->first) % storage.size();
             iter.push_back(*it);
             ++sz;
-            base[key_hash].push_back(--iter.end());
+            storage[key_hash].push_back(--iter.end());
         }
     }
-    HashMap(const std::initializer_list<std::pair<const KeyType, ValueType>> & init_list,
+    HashMap(const std::initializer_list<Key_Value> & init_list,
             Hash _hasher = Hash()) 
         : hasher(_hasher) {
-        base = std::vector<std::vector<typename std::list<std::pair<const KeyType, ValueType>>::iterator>>(HashMapBaseSize);
+        storage = HashMapStorage(HashMapStorageSize);
         for (auto it : init_list) {
-            size_t key_hash = hasher(it.first) % base.size();
+            size_t key_hash = hasher(it.first) % storage.size();
             iter.push_back(it);
             ++sz;
-            base[key_hash].push_back(--iter.end());
+            storage[key_hash].push_back(--iter.end());
         }
     }
+
 
     size_t size() const{
         return sz;
@@ -59,100 +65,82 @@ class HashMap {
     const Hash& hash_function() const {
         return hasher;
     }
-    void clear(size_t __k = HashMapBaseSize) {
+    void clear(size_t k = HashMapStorageSize) {
         iter.clear();
-        base.clear();
+        storage.clear();
         sz = 0;
-        base.resize(__k);
-    }
-    void reload_base() {
-        if (sz > 2 * base.size()) {
-            base.resize(3 * sz);
-        }
-        for (auto i = iter.begin(); i != iter.end(); ++i) {
-            size_t key_hash = hasher(i->first) % base.size();
-            base[key_hash].push_back(--iter.end());
-        }
+        storage.resize(k);
     }
     void rebuild() {
-        size_t ns = 2 * base.size();
-        base.clear();
-        base.resize(ns);
-        reload_base();
-    }
-    /*
-    void rebuild() {
-        std::list<std::pair<const KeyType, ValueType>> iter2 = iter;
-        clear(base.size() * 2);
+        HashMapList iter2 = iter;
+        clear(storage.size() * StorageResizeMultiplier);
         for (auto i : iter2) {
             insert(i);
         }
     }
-    //*/
-    void insert(const std::pair<const KeyType, ValueType> & p) {
-        //std::cerr << p.first << " + " << p.second << std::endl;
-        size_t key_hash = hasher(p.first) % base.size();
-        for (auto elem : base[key_hash]) {
+    void insert(const Key_Value & p) {
+        size_t key_hash = hasher(p.first) % storage.size();
+        for (auto elem : storage[key_hash]) {
             if (elem->first == p.first) {
                 return;
             }
         }
         iter.push_back(p);
         ++sz;
-        base[key_hash].push_back(--iter.end());
-        if (2 * sz > base.size()){
+        storage[key_hash].push_back(--iter.end());
+        if (StorageResizeMultiplier * sz > storage.size()){
             rebuild();
         }
     }
     void erase(const KeyType & key) {
-        size_t key_hash = hasher(key) % base.size();
+        size_t key_hash = hasher(key) % storage.size();
         size_t i = 0;
-        while (i < base[key_hash].size() && !(base[key_hash][i]->first == key)) {
+        while (i < storage[key_hash].size() && !(storage[key_hash][i]->first == key)) {
             ++i;
         }
-        if (i != base[key_hash].size()) {
-            iter.erase(base[key_hash][i]);
+        if (i != storage[key_hash].size()) {
+            iter.erase(storage[key_hash][i]);
             --sz;
-            base[key_hash].erase(base[key_hash].begin() + i);
+            storage[key_hash].erase(storage[key_hash].begin() + i);
         }
     }
 
     const_iterator begin() const {
         return iter.cbegin();
     }
-    iterator begin() {
+    iterator begin(){
         return iter.begin();
     }
     const_iterator end() const {
         return iter.cend();
     }
-    iterator end() {
+    iterator end(){
         return iter.end();
     }
 
     iterator find(const KeyType& key) {
-        size_t key_hash = hasher(key) % base.size();
+        size_t key_hash = hasher(key) % storage.size();
         size_t i = 0;
-        while (i < base[key_hash].size() && !(base[key_hash][i]->first == key)) {
+        while (i < storage[key_hash].size() && !(storage[key_hash][i]->first == key)) {
             ++i;
         }
-        if (i == base[key_hash].size()) {
+        if (i == storage[key_hash].size()) {
             return end();
         }
         else {
-            return base[key_hash][i];
+            return storage[key_hash][i];
         }
     }
     const_iterator find(const KeyType& key) const {
-        size_t key_hash = hasher(key) % base.size();
+        size_t key_hash = hasher(key) % storage.size();
         size_t i = 0;
-        while (i < base[key_hash].size() && !(base[key_hash][i]->first == key)) {
+        while (i < storage[key_hash].size() && !(storage[key_hash][i]->first == key)) {
             ++i;
         }
-        if (i == base[key_hash].size()) {
+        if (i == storage[key_hash].size()) {
             return end();
         } else {
-            return base[key_hash][i];
+            return storage[key_hash][i];
         }
     }
 
@@ -167,7 +155,6 @@ class HashMap {
         }
     }
 
-
     const ValueType& at(const KeyType & key) const{
         const_iterator fnd = find(key);
         if (fnd != end()) {
@@ -177,18 +164,10 @@ class HashMap {
         }
     }
 
+
     HashMap& operator= (const HashMap& hm) {
-        /*
-        hasher = hm.hasher;
-        sz = hm.sz;
-        iter.clear();
-        for (auto p : hm.iter) {
-            iter.push_back(p);
-        }
-        reload_base();
-        */
-        std::list<std::pair<const KeyType, ValueType>> hmiter = hm.iter;
-        clear(std::max(3 * hm.sz, HashMapBaseSize));
+        HashMapList hmiter = hm.iter;
+        clear(std::max(StorageResizeMultiplier * sz, HashMapStorageSize));
         hasher = hm.hasher;
         for (auto p : hmiter) {
             insert(p);
@@ -196,4 +175,3 @@ class HashMap {
         return *this;
     }
 };
-
